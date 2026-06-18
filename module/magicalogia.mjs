ca16a123e9624a9b6c110bc7ac87410870ba4b85
JBGeum <gysophila001@gmail.com>
2026-06-18 14:32:22 +0900

    feat: add ApplicationV2 sheets, templates, and sheet registration

--- module/magicalogia.mjs
+++ module/magicalogia.mjs
@@ -7,6 +7,9 @@ import { MagicalogiaItem } from "./documents/item.mjs";
 // DataModels
 import { CharacterDataModel } from "./data/actors/character.mjs";
 import { GenericItemDataModel } from "./data/items/generic.mjs";
+// Sheets
+import { MagicalogiaActorSheet } from "./sheets/actor-sheet.mjs";
+import { MagicalogiaItemSheet } from "./sheets/item-sheet.mjs";
 // Helpers
 import { MAGICALOGIA } from "./helpers/config.mjs";
 import { preloadHandlebarsTemplates, registerHandlebarsHelpers } from "./helpers/templates.mjs";
@@ -28,6 +31,15 @@ Hooks.once("init", async function () {
   CONFIG.Item.dataModels = {
     generic: GenericItemDataModel,
   };
+
+  // V13: 시트 컬렉션은 foundry.documents.collections 네임스페이스.
+  const ActorsCls = foundry.documents.collections.Actors;
+  const ItemsCls = foundry.documents.collections.Items;
+
+  ActorsCls.unregisterSheet("core", foundry.appv1.sheets.ActorSheet);
+  ActorsCls.registerSheet("magicalogia", MagicalogiaActorSheet, { makeDefault: true });
+  ItemsCls.unregisterSheet("core", foundry.appv1.sheets.ItemSheet);
+  ItemsCls.registerSheet("magicalogia", MagicalogiaItemSheet, { makeDefault: true });
 
   registerHandlebarsHelpers();
   return preloadHandlebarsTemplates();
